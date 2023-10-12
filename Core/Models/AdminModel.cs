67f36b03b31e60d0fcdc8e57f02f8a45a461bb2e
M K V Kishore <36737425+kishoremkv@users.noreply.github.com>
2023-10-12 11:58:59 +0530

    Added model class

---
+++ Core/Models/AdminModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+	public class AdminModel
+	{
+	
+		public string email { get; set; }
+
+		public string password { get; set; }
+		
+		public string role { get; set; }
+	}
+}
